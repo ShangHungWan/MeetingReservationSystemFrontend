c5454114d9a546e6f61d7a34f72400d11560f004
Sun <a24230928@gmail.com>
2021-01-03 20:17:56 +0800

    feat: 預約表單及查看會議詳細資料

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './css/bootstrap.min.css';
 import './css/Header.css';
 import Reservation from './Reservation';
 import MyMeeting from './MyMeeting';
@@ -11,15 +10,27 @@ class Header extends React.Component {
     render() {
         return (
             <Router>
-                <header className="d-flex flex-column flex-md-row align-items-center p-4 px-md-5 mb-4 bg-white border-bottom shadow-sm">
-                    <p className="h4 my-0 me-md-auto fw-normal">空間管理系統</p>
-                    <nav className="my-2 my-md-0 me-md-3">
-                        <Link to="/reservation" className="p-2 text-dark navbar-brand">預約狀況</Link>
-                        <Link to="/myMeeting" className="p-2 text-dark navbar-brand">我的會議</Link>
-                    </nav>
-                    <Link to="/login" className="mx-2 btn btn-outline-primary">Log in</Link>
-                    <Link to="/signup" className="mx-2 btn btn-primary">Sign up</Link>
-                </header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light p-4 border-bottom shadow-sm">
+                    <Link to="/" className="navbar-brand">空間管理系統</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to="/reservation" className="p-2 text-dark navbar-brand">預約狀況</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/myMeeting" className="p-2 text-dark navbar-brand">我的會議</Link>
+                            </li>
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                            <Link to="/login" className="mx-2 btn btn-outline-primary">Log in</Link>
+                            <Link to="/signup" className="mx-2 btn btn-primary">Sign up</Link>
+                        </form>
+                    </div>
+                </nav>
                 <Switch>
                     <Route path="/" exact component={Reservation} />
                     <Route path="/reservation" component={Reservation} />
--- src/Reservation.js
+++ src/Reservation.js
@@ -4,10 +4,18 @@ import React from 'react';
 class Reservation extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { date: new Date().toISOString().slice(0, 10), rooms: null, reservations: null, isLoading: false, isChanged: false };
+        this.state = {
+            date: new Date().toISOString().slice(0, 10),
+            rooms: null,
+            reservations: null,
+            isLoading: false,
+            isChanged: false
+        };
 
         this.startTimeHour = 9;
         this.handleChange = this.handleChange.bind(this);
+        this.showReservation = this.showReservation.bind(this);
+        this.showNewReservation = this.showNewReservation.bind(this);
     }
 
     getRoomList() {
@@ -66,21 +74,21 @@ class Reservation extends React.Component {
             for (let j = 0; j < this.state.rooms.length; j++) {
                 let find = false;
                 if (this.state.reservations) {
-                    for (let k of this.state.reservations) {
-                        const start = new Date(k.start);
-                        const end = new Date(k.end);
-                        if (start.getHours() <= (i + this.startTimeHour) && end.getHours() <= (i + this.startTimeHour + 1) && k.room_id === this.state.rooms[j]._id) {
+                    this.state.reservations.forEach((ele, idx) => {
+                        const start = new Date(ele.start);
+                        const end = new Date(ele.end);
+                        if (start.getHours() <= (i + this.startTimeHour) && end.getHours() <= (i + this.startTimeHour + 1) && ele.room_id === this.state.rooms[j]._id) {
                             status.push(<td key={`${i}-${j}`} className="table-active">
-                                <button className="btn btn-link">{k.topic}</button>
+                                <button className="btn btn-link" data-toggle="modal" data-target="#reservationDetail" data-reservation={idx} onClick={this.showReservation}>{ele.topic}</button>
                             </td>);
                             find = true;
-                            break;
+                            return;
                         }
-                    }
+                    });
                 }
                 if (!find) {
                     status.push(<td key={`${i}-${j}`}>
-                        <button className="btn btn-outline-success">預約</button>
+                        <button className="btn btn-outline-success" data-toggle="modal" data-target="#newReservation" onClick={this.showNewReservation} data-room={`${i}-${j}`}>預約</button>
                     </td>);
                 }
             }
@@ -93,15 +101,72 @@ class Reservation extends React.Component {
         return timeList;
     }
 
+    getRoomName(id) {
+        for (let ele of this.state.rooms) {
+            if (ele._id === id) {
+                return ele.name;
+            }
+        }
+    }
+
+    showReservation(event) {
+        const idx = parseInt(event.target.dataset.reservation);
+        const reservation = this.state.reservations[idx];
+
+        const spans = document.querySelectorAll('#reservationDetail > div > div > div:nth-child(2) > div span');
+        spans[0].innerHTML = reservation.host_id;
+        spans[1].innerHTML = this.getRoomName(reservation.room_id);
+        spans[2].innerHTML = reservation.detail;
+        spans[3].innerHTML = `${reservation.start.slice(0, 19)} ~ ${reservation.end.slice(0, 19)}`;
+
+        const header = document.querySelector('#reservationDetail > div > div > div:nth-child(1) > h5 > span')
+        header.innerHTML = reservation.topic;
+    }
+
+    showNewReservation(event) {
+        const idx = event.target.dataset.room.split('-');
+        const time = this.startTimeHour + parseInt(idx[0]);
+        const room = this.state.rooms[idx[1]];
+
+        this.generateTimeOption(idx);
+
+        document.querySelector('#start').value = `${document.querySelector('#date').value} ${time}:00`;
+        document.querySelector('#room').value = room.name;
+    }
+
+    generateTimeOption(idx) {
+        const maxTime = this.getMaxTime(idx);
+
+        document.querySelector('#time').innerHTML = "";
+        for (let i = 1; i <= maxTime; i++) {
+            let newOption = document.createElement("option");
+            newOption.value = i;
+            newOption.innerHTML = `${i} 小時`;
+            document.querySelector('#time').appendChild(newOption);
+        }
+    }
+
+    getMaxTime(idx) {
+        let counter = 1;
+        for (let i = 1; i <= 2; i++) {
+            const nextTime = document.querySelector(`[data-room="${parseInt(idx[0]) + i}-${idx[1]}"]`);
+            if (nextTime) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
     handleChange(event) {
         this.setState({ date: event.target.value, isChanged: true })
     }
 
     showLoading() {
-        return (
-            <div className="container p-12 text-center position-absolute top-50 start-50 translate-middle">
-                <h1>loading...</h1>
-            </div>);
+        return (<div className="d-flex p-5">
+            <div className="d-flex w-100 justify-content-center align-self-center">
+                <h1>Loading...</h1>
+            </div>
+        </div>);
     }
 
     render() {
@@ -121,7 +186,7 @@ class Reservation extends React.Component {
 
         return (
             <div className="container p-4">
-                <input className="col-auto my-3 form-control" type="date" defaultValue={this.state.date} onChange={this.handleChange} />
+                <input className="col-auto my-3 form-control" id="date" type="date" defaultValue={this.state.date} onChange={this.handleChange} />
                 <div className="table-responsive">
                     <table className="table my-2 text-center">
                         <thead>
@@ -134,6 +199,86 @@ class Reservation extends React.Component {
                             {time}
                         </tbody>
                     </table>
+                </div>
+
+                <div className="modal fade" id="newReservation" tabIndex="-1" aria-labelledby="reservationLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="reservationLabel">預約</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label htmlFor="title">標題</label>
+                                        <input type="text" className="form-control" id="title" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="detail">描述</label>
+                                        <input type="text" className="form-control" id="detail" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="room">會議室</label>
+                                        <input type="text" readOnly className="form-control" id="room" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="start">開始時間</label>
+                                        <input type="text" readOnly className="form-control" id="start" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="time">持續時間</label>
+                                        <select className="form-control" id="time">
+                                            <option value="1">1 小時</option>
+                                            <option value="2">2 小時</option>
+                                            <option value="3">3 小時</option>
+                                        </select>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-primary">Submit</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="modal fade" id="reservationDetail" tabIndex="-1" aria-labelledby="reservationDetailLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="reservationDetailLabel">詳細資訊 - <span></span></h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="form-group row">
+                                    <label className="col-sm-3 col-form-label">主辦人：</label>
+                                    <span className="col-sm-10 col-form-label"></span>
+                                </div>
+                                <div className="form-group row">
+                                    <label className="col-sm-3 col-form-label">會議室：</label>
+                                    <span className="col-sm-10 col-form-label"></span>
+                                </div>
+                                <div className="form-group row">
+                                    <label className="col-sm-3 col-form-label">描述：</label>
+                                    <span className="col-sm-10 col-form-label"></span>
+                                </div>
+                                <div className="form-group row">
+                                    <label className="col-sm-3 col-form-label">時間：</label>
+                                    <span className="col-sm-10 col-form-label"></span>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-primary">Join</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
